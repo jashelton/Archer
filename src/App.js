acfa26a07805a6c4ae26ea454c199cde57008a3c
jashelton <justin.shelton@yahoo.com>
2018-01-07 00:14:40 -0500

    adding home component and basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+// Routes
+import Home from './home';
+
+import { Switch, Route } from 'react-router';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header text-center">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>Archer</h2>
+        </div>
+        <div className="app-container">
+          <Switch>
+            <Route exact path="/home" component={Home} />
+          </Switch>
+        </div>
       </div>
     );
   }
