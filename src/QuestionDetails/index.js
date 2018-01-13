75143dfb2576493bcebe71f0c90116083da0b52b
jashelton <justin.shelton@yahoo.com>
2018-01-13 02:18:09 -0500

    adding answers to question-details

--- src/QuestionDetails/index.js
+++ src/QuestionDetails/index.js
@@ -16,6 +16,10 @@ class QuestionDetailsPage extends React.Component {
         {question.items &&
           <div>
             <h1>{question.items.primary_question.question}</h1>
+            {question.items.primary_question.answers.map(a => (
+              <span key={a.id}>{a.answer}</span>
+            ))}
+            {/* Create a question component.  Map over and render it with the answers */}
             <ul>
               {question.items.secondary_questions.map(q => (
                 <li key={q.id}>{q.question}</li>
